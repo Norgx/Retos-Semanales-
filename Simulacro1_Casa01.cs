e5fee3a82531a4d5446ff7e97aa0db6bbe1dadfa
Yocelin <77703713+Norgx@users.noreply.github.com>
2021-02-16 00:12:48 -0500

    Add files via upload

---
+++ Simulacro1_Casa01.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulacro1_Ciclos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int dado1 = 0, dado2 = 0;
+            int sumados = dado1 + dado2;
+            double turno = 0;
+            int tmayorseis = 0;
+            int dobles = 0;
+
+            Console.WriteLine("Desea iniciar el juego : (s / n)");
+            string respuesta = Console.ReadLine();
+
+            while ((respuesta == "s")) 
+            {
+                dado1 = aleatorio.Next(1, 7);
+                dado2 = aleatorio.Next(1, 7);
+
+                if (dado1 == dado2)
+                {
+                    dobles++;
+                }
+                if ((dado1 ==1) && dado2 ==1)
+                {
+                    sumados = 0;
+                    Console.WriteLine("Usted ha sido eliminado" + ", \n Dado 1 = " + dado1 + ", \n Dado 2 = " + dado2);
+                    respuesta = "no";
+                }
+                else if ( dobles == 3)
+                {
+                    Console.WriteLine("Felicidades, ha ganado el juego");
+                    respuesta = "no";
+                }
+                else
+                {
+                    sumados += dado1 + dado2;
+                    Console.WriteLine("Dado 1 : " + dado1 + ",\n Dado 2 : " + dado2 + ", \n Total = " + sumados);
+
+
+                    if (sumados >= 100)
+                    {
+                        Console.WriteLine("Felicidades; ha ganado el juego ");
+                        respuesta = "no";
+                        break;
+                    }
+                    Console.WriteLine("¿Continuar? ");
+                    respuesta = Console.ReadLine();
+                }
+
+                turno++;
+                
+                if (sumados > 6)
+                {
+                    tmayorseis++;
+                }
+
+                Console.WriteLine("Contador = " + tmayorseis);
+                double porcentaje = (tmayorseis / turno) * 100;
+
+                Console.WriteLine("Número de partidas: " + turno + "\n Porcentaje = " + porcentaje);
+                Console.WriteLine("Gracias por jugar");
+                respuesta = "no";
+            }
+            // video: https://youtu.be/a6i_7sB-ONU 
+
+
+        }
+    }
+}
---
+++ Valores Extremos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valores_Extremos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0; //resultado 
+            Console.WriteLine(" Ingrese el número de datos (n): ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, max = 0, min = 200;
+            string nombreMax = "";
+            string nombreMin = "";
+
+
+
+            while (i < n) 
+            {
+                Console.WriteLine("Ingrese el nombre y la edad: ");
+                string nombre = Console.ReadLine();
+                int edad = int.Parse(Console.ReadLine());
+
+                if (edad > max) 
+                {
+                    max = edad;
+                    nombreMax = nombre;
+                    
+                }
+                if (edad < min)
+                {
+                    min = edad;
+                    nombreMin = nombre;
+                }
+
+                total += edad;
+
+                i++;
+
+
+            }
+            double promedio = total / n;
+            Console.WriteLine("Promedio = " + promedio);
+            Console.WriteLine("Mayor = "  + max + "," + nombreMax);
+            Console.WriteLine("Minimo = " + min + "," + nombreMin);
+        }
+    }
+}
+//vid: https://youtu.be/cPdHZufvGyo
